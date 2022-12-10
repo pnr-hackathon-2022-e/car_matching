f77fac8778ba0f8426a30f0d9ec66aea0568c7e5
yuuumbk <hikoki.y@gmail.com>
2022-12-10 13:47:09 +0900

    test: 通話テスト

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { Avatar, Box, Container, Stack, Typography } from "@mui/material";
-import { useCallback, useContext, useState } from "react";
+import { useCallback, useContext, useEffect, useRef, useState } from "react";
 import { UserEditModal } from "../components/UserEditModal";
 import { userContext } from "../contexts/UserContext";
 
@@ -10,13 +10,52 @@ import FriendButton from "../assets/home/friend_button.png";
 import Title from "../assets/home/title.png";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import Peer from "skyway-js";
 
 export const Home = () => {
   const user = useContext(userContext);
   const [open, setOpen] = useState(false);
   const [isMatching, setIsMatching] = useState(false);
   const navigate = useNavigate();
-  axios.defaults.headers.common["content-type"] = "application/json";
+  const videoRef = useRef(null);
+  const theirVideoRef = useRef(null);
+  const [myId, setMyId] = useState();
+  const [theirId, setTheirId] = useState();
+  // axios.defaults.headers.common["content-type"] = "application/json";
+  const peer = new Peer({
+    key: "ee6b9a84-7ce4-4507-afd0-73008af50e2b",
+  });
+  let localStream;
+
+  const setEventListener = (mediaConnection) => {
+    mediaConnection.on("stream", (stream) => {
+      theirVideoRef.current.srcObject = stream;
+      theirVideoRef.current.play();
+    });
+  };
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ video: false, audio: true })
+      .then((stream) => {
+        videoRef.current.srcObject = stream;
+        videoRef.current.play();
+        localStream = stream;
+      })
+      .catch((error) => {
+        console.error("mediaDevice.getUserMedia() error:", error);
+        return;
+      });
+
+    peer.on("open", () => {
+      setMyId(peer.id);
+    });
+
+    peer.on("call", (mediaConnection) => {
+      mediaConnection.answer(localStream);
+      setEventListener(mediaConnection);
+    });
+  }, []);
 
   const handleClickEditButton = useCallback(() => {
     setOpen(!open);
@@ -24,26 +63,31 @@ export const Home = () => {
 
   const handleClickCarButton = useCallback(async () => {
     setIsMatching(!isMatching);
-    await axios
-      .post(
-        "https://34.125.227.230:8000/caller1/",
-        {
-          name: "Title",
-          pass: "Body",
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-          },
-        }
-      )
-      .then((res) => console.log(res));
-    // .catch((e) => console.log(e));
-    await axios
-      .get("https://api.sampleapis.com/beers/ale")
-      .then((res) => console.log(res))
-      .catch((e) => console.log(e));
+    const mediaConnection = peer.call(
+      theirId ?? "9CubKYWBsLAMv7So",
+      localStream
+    );
+    setEventListener(mediaConnection);
+    // await axios
+    //   .post(
+    //     "http://34.125.227.230:8000/caller1/",
+    //     {
+    //       name: "Title",
+    //       pass: "Body",
+    //     },
+    //     // {
+    //     //   headers: {
+    //     //     "Content-Type": "application/json",
+    //     //     "Access-Control-Allow-Origin": "*",
+    //     //   },
+    //     // }
+    //   )
+    //   .then((res) => console.log(res))
+    //   .catch((e) => console.log(e));
+    // await axios
+    //   .get("https://reqres.in/api/users?page=2")
+    //   .then((res) => console.log(res))
+    //   .catch((e) => console.log(e));
     // await fetch("http://34.125.227.230:8000/caller1", {
     //   method: "POST",
     //   body: JSON.stringify({ name: "Title", pass: "Body" }),
@@ -55,7 +99,7 @@ export const Home = () => {
     setTimeout(() => {
       navigate("/matching");
     }, 2000);
-  }, [isMatching]);
+  }, [isMatching, peer]);
 
   const handleClickFriendButton = useCallback(() => {
     navigate("/friends");
@@ -73,6 +117,13 @@ export const Home = () => {
         width: "100%",
       }}
     >
+      <>myId: {myId}</>
+      <br />
+      <>theirId: </>
+      <input
+        value={theirId}
+        onChange={(event) => setTheirId(event.target.value)}
+      />
       <Box sx={{ textAlign: "center", mt: 5 }}>
         <img
           src={Title}
@@ -124,6 +175,21 @@ export const Home = () => {
         </Stack>
       </Stack>
       <UserEditModal open={open} setOpen={setOpen} />
+      <video
+        ref={videoRef}
+        id="my-video"
+        // width="400px"
+        autoPlay
+        muted
+        playsinline
+      ></video>
+      <video
+        ref={theirVideoRef}
+        id="their-video"
+        // width="400px"
+        autoPlay
+        playsinline
+      ></video>
     </Container>
   );
 };
